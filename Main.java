c0b914805fe193f68e6626b5797e9a92e3243c8a
O_glt <giraulto@3il.fr>
2026-01-28 14:06:46 +0100

    add green string

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenue sur JavaPass");
+        System.out.println(GREEN + "Bienvenue sur JavaPass");
     }
+    public static final String GREEN = "\033[0;32m"; //Green
 }
