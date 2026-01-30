7e955869067024726d927e666bf9b33b7267deea
O_glt <giraulto@3il.fr>
2026-01-30 09:33:28 +0100

    Affichage fonctionnalité ajoutée et message de bienvenue coloré implémenté.

---
+++ Affichage.java
@@ -0,0 +1,7 @@
+public class Affichage {
+    public static final String GREEN = "\033[0;32m"; //Green
+
+    public static void afficherBienvenue() {
+        System.out.println(GREEN + "Bienvenue sur JavaPass !");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(GREEN + "Bienvenue sur JavaPass !");
+        Affichage.afficherBienvenue();
     }
-    public static final String GREEN = "\033[0;32m"; //Green
 }
