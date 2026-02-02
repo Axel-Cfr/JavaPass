9b020e05dcae27e236e5501162e568888e321759
O_glt <giraulto@3il.fr>
2026-02-02 14:52:08 +0100

    changes

--- src/Affichage.java
+++ src/Affichage.java
@@ -3,6 +3,6 @@ public class Affichage {
 
     public static void afficherBienvenue() {
         System.out.println(GREEN + "Bienvenue sur JavaPass !");
-        System.out.println("Created by Axel Chabot, Morgann Morvan, and Octave Girault");
+        System.out.println("Created by Axel Chabot, Morgann Morvan and Octave Girault");
     }
 }
