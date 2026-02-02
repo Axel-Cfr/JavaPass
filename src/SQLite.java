a4dad237aa396062c64298783202c9703ed786e9
O_glt <giraulto@3il.fr>
2026-02-02 22:49:37 +0100

    ajout de la base de données SQLite et des schémas JDBC

--- src/SQLite.java
+++ src/SQLite.java
@@ -1,6 +1,32 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 //https://www.youtube.com/watch?v=TN_xTjbrzzc
+//SQLite permet de stocker des données localement fournis avec l'application.
+//utilise Java Database Connectivity (JDBC) API pour se connecter à une base de données SQLite.
+//Avec un système de pilotes, le jdbc.jar est notre pilote.
 public class SQLite {
-    
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static void initialisationDB() //On etablis la connection avec la base de donnees
+    {
+        try{
+            Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
+            Statement stmt = co.createStatement();
+            System.out.println("Connexion a la base de donnees etablie");
+        }
+        catch(SQLException e)// On gere les exceptions liees a la base de donnees (tres important))
+        {
+            System.err.println(e.getMessage());// message d'erreur
+        }
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static void ajoutTable()//On cree une table dans la base de donnees
+    {
+        //not finished yet
+    }
 }
