6f400af8c4aee15bbb3edf8ea83ad32e7575918a
Axel-Cfr <chabota@3il.fr>
2026-02-04 22:27:45 +0100

    Création de l'architecture Maven

--- src/Affichage.java
+++
@@ -1,8 +0,0 @@
-public class Affichage {
-    public static final String GREEN = "\033[0;32m"; //Green
-
-    public static void afficherBienvenue() {
-        System.out.println(GREEN + "Bienvenue sur JavaPass !");
-        System.out.println("Created by Axel Chabot, Morgann Morvan and Octave Girault");
-    }
-}
