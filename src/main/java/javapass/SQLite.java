023f411e6208c32b12756b0f03f11cd74112aef0
O_glt <giraulto@3il.fr>
2026-03-11 09:41:08 +0100

    ajout get_user et get_mdp

--- src/main/java/javapass/SQLite.java
+++ src/main/java/javapass/SQLite.java
@@ -3,6 +3,7 @@ package javapass;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -92,10 +93,119 @@ public class SQLite {
             System.err.println(e.getMessage());
         }
     }
+    public static void ajout_mdp(int user_id, String website_name, String url, String encrypted_username, byte[] iv_username, byte[] iv_password) { //On ajout un mdp dans la base de donnees/ watchout aux type des parametres
+        try {
+            Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
+            String sql = "INSERT INTO passwords VALUES (null,?,?,?,?,?)";
+            PreparedStatement pstmt = co.prepareStatement(sql);
+            pstmt.setInt(1, user_id);
+            pstmt.setString(2, website_name);
+            pstmt.setString(3, url);
+            pstmt.setString(4, encrypted_username);
+            pstmt.setBytes(5, iv_username);
+            pstmt.setBytes(6, iv_password);
+            
+            pstmt.executeUpdate();
+            pstmt.close();
+            co.close();
+            System.out.println("Mot de passe ajouté avec succès");
+        } 
+        catch (SQLException e) 
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+    public ReturningUserValues get_user(String username){
+        try {
+            Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
+            String sql = "SELECT * FROM users WHERE username = ?";
+            PreparedStatement pstmt = co.prepareStatement(sql);
+            pstmt.setString(1,username);
+            ResultSet rs = pstmt.executeQuery(sql);
+
+            //while(rs.next()) est utilisé dans certain cas, mais pas ici au cas ou je le laisse
+            int user_id = rs.getInt("user_id");
+            //on a deja username en parametre
+            String  encrypted_textAndTag_verify = rs.getString("encrypted_textAndTag_verify");
+            byte[] iv_verify = rs.getBytes("iv_verify");
+            byte[] salt = rs.getBytes("salt");
+            String last_login = rs.getString("last_login");
+
+            ReturningUserValues rv = new ReturningUserValues(user_id, username, encrypted_textAndTag_verify, iv_verify, salt, last_login);
+            
+            pstmt.close();
+            co.close();
+            return rv;
+        } 
+        catch (SQLException e)//throw to services a^pres 
+        {
+            System.err.println(e.getMessage());
+            return null;
+        }
+    }
+    public final class ReturningUserValues{
+        private final int user_id;
+        private final String username;
+        private final String encrypted_textAndTag_verify;
+        private final byte[] iv_verify;
+        private final byte[] salt;
+        private final String last_login;
+        public ReturningUserValues(int user_id, String username, String encrypted_textAndTag_verify, byte[] iv_verify, byte[] salt, String last_login){
+            this.user_id = user_id;
+            this.username = username;
+            this.encrypted_textAndTag_verify = encrypted_textAndTag_verify;
+            this.iv_verify = iv_verify;
+            this.salt = salt;
+            this.last_login = last_login;
+        }
+        //peutetre ajouter getters
+    }
+    public ReturningMdpValues get_mdp(int user_id){
+        try {
+            Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
+            String sql = "SELECT * FROM passwords WHERE user_id = ?";
+            PreparedStatement pstmt = co.prepareStatement(sql);
+            pstmt.setInt(1,user_id);
+            ResultSet rs = pstmt.executeQuery(sql);
+
+            //while(rs.next()) est utilisé dans certain cas, mais pas ici au cas ou je le laisse
+            int password_id = rs.getInt("password_id");
+            //on a deja user_id en parametre
+            String website_name = rs.getString("website_name");
+            String url = rs.getString("url");
+            String encrypted_username = rs.getString("encrypted_username");
+            byte[] iv_username = rs.getBytes("iv_username");
+            byte[] iv_password = rs.getBytes("iv_password");
+
+            ReturningMdpValues rv = new ReturningMdpValues(password_id, user_id, website_name, url, encrypted_username, iv_username, iv_password);
+            
+            pstmt.close();
+            co.close();
+            return rv;
+        } 
+        catch (SQLException e)//throw to services a^pres 
+        {
+            System.err.println(e.getMessage());
+            return null;
+        }
+    }
+    public final class ReturningMdpValues{
+        private final int password_id;
+        private final int user_id;
+        private final String website_name;
+        private final String url;
+        private final String encrypted_username;
+        private final byte[] iv_username;
+        private final byte[] iv_password;
+        public ReturningMdpValues(int password_id, int user_id, String website_name, String url, String encrypted_username, byte[] iv_username, byte[] iv_password){
+            this.password_id = password_id;
+            this.user_id = user_id;
+            this.website_name = website_name;
+            this.url = url;
+            this.encrypted_username = encrypted_username;
+            this.iv_username = iv_username;
+            this.iv_password = iv_password;
+        }
+        //peutetre ajouter getters
+    }
 }
-/*  
-on va créer des méthodes permetant de recup : 
-encrypted_textAndTag_verify
-iv_verify
-salt
-*/
