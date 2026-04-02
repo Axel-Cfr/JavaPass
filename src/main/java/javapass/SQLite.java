9344ba582c64498baf0e2ae12f087732e22f9519
O_glt <giraulto@3il.fr>
2026-04-02 09:07:33 +0200

    Commencement commentaire SQLite + reparation ajout mdp

--- src/main/java/javapass/SQLite.java
+++ src/main/java/javapass/SQLite.java
@@ -1,5 +1,8 @@
 package javapass;
 
+/*══════════════════════════════════════════════════════════════════════════════════════════════════════════════
+Importation des outils nécessaires
+*/
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,25 +11,44 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-//https://www.youtube.com/watch?v=TN_xTjbrzzc
-//SQLite permet de stocker des données localement fournis avec l'application.
-//utilise Java Database Connectivity (JDBC) API pour se connecter à une base de données SQLite.
-//Avec un système de pilotes, le jdbc.jar est notre pilote.
+/*══════════════════════════════════════════════════════════════════════════════════════════════════════════════
+Sources :
 
-public class SQLite {
-    /*public static void main(String[] args) {
-        initialisationDB();
-        ajoutTable_base();
-    }*/
+https://sqlite.org/docs.html
+https://www.youtube.com/watch?v=TN_xTjbrzzc
+utilisation de Java Database Connectivity (JDBC) API pour se connecter à une base de données SQLite.
+Avec un système de pilotes, le jdbc.jar est notre pilote (au sein du pom.xml). 
 
-    public void initialisationDB() throws SQLException { //On etablis la connection avec la base de donnees
+*//*════════════════════════════════════════════════════════════════════════════════════════════════════════════
+Explication de la Class :
+
+Class SQLite utilisé de façon à gérer toutes les données utilisateurs dans l'application
+Des chiffrements AES et ARGON2 sont implémentés afin que toutes les données soient sécurisées
+
+══════════════════════════════════════════════════════════════════════════════════════════════════════════════*/
+
+
+public class SQLite 
+{
+    /* 
+    On établis la connection avec la base de données
+    et lui ajoute les tables users et passwords
+    si elles n'existent pas. 
+    */
+    public void initialisationDB() throws SQLException 
+    { 
         Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
         System.out.println("Connexion a la base de donnees etablie");
         ajoutTable_base();
     }
 
-    public void ajoutTable_base() throws SQLException { //On cree les tables dans la base de donnees si elles n'existent pas
-
+    /* 
+    Cette méthode permet d'ajouter les tables de base à la base de données
+    Seulement si celles-ci n'existent pas encore
+    Elle est utilisé dans l'initialisation de l'application 
+    */
+    public void ajoutTable_base() throws SQLException 
+    {
         Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
         Statement stmt = co.createStatement();
         String sql = "CREATE TABLE IF NOT EXISTS users (" +
@@ -57,14 +79,27 @@ public class SQLite {
                     created_at TEXT,
                     updated_at TEXT,
                     */
-                        
+                    
         stmt.executeUpdate(sql);
         stmt.close();
         co.close();
         System.out.println("Tables créées avec succès");
     }
 
-    public void ajout_utilisateur(String username, String  encrypted_textAndTag_verify, byte[] iv_verify, byte[] salt, int argon2Type, String last_login) throws SQLException { //On ajout un utilisateur dans la base de donnees/ watchout aux type des parametres
+    /* 
+    Cette méthode ajoute un utilisateur dans la table users
+    pour ce faire il faut renseigner :
+        - un nom d'utilisateur
+        - un chiffrement du nom d'utilisateur (pour les comparaisons)   ═╗    
+        - un vecteur d'initialisation                                    ║ 
+        - un sel (gérer les chiffrements)                                ╠═> géré de façon automatique
+        - le type d'argon2                                               ║ 
+        - le last_login                                                 ═╝
+    */
+    public void ajout_utilisateur(String username, String  encrypted_textAndTag_verify, 
+                                byte[] iv_verify, byte[] salt, int argon2Type, 
+                                String last_login) throws SQLException 
+    {
         Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
         String sql = "INSERT INTO users VALUES (null,?,?,?,?,?,?)";
         PreparedStatement pstmt = co.prepareStatement(sql);
@@ -81,10 +116,20 @@ public class SQLite {
         //System.out.println("utilisateur ajouté avec succès");
     }
 
-    public static void ajout_mdp(int user_id, String website_name, String url, String encrypted_username, String encrypted_password, byte[] iv_username, byte[] iv_password) throws SQLException {
-        //On ajout un mdp dans la base de donnees
+
+
+
+    /* 
+    Cette méthode ajoute à la base de données
+    */
+    public void ajout_mdp(int user_id, String website_name, 
+                                String url, String encrypted_username, 
+                                String encrypted_password, byte[] iv_username, 
+                                byte[] iv_password) throws SQLException 
+        {
+        
         Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
-        String sql = "INSERT INTO passwords VALUES (null,?,?,?,?,?,?)";
+        String sql = "INSERT INTO passwords VALUES (null,?,?,?,?,?,?,?)";
         PreparedStatement pstmt = co.prepareStatement(sql);
         pstmt.setInt(1, user_id);
         pstmt.setString(2, website_name);
@@ -100,7 +145,8 @@ public class SQLite {
         System.out.println("Mot de passe ajouté avec succès");
     }
 
-    public UserValues get_user(String username) throws SQLException {
+    public UserValues get_user(String username) throws SQLException 
+    {
         Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
         String sql = "SELECT * FROM users WHERE username = ?";
         PreparedStatement pstmt = co.prepareStatement(sql);
@@ -123,7 +169,8 @@ public class SQLite {
         return rv;
     }
 
-    public final class UserValues {
+    public final class UserValues 
+    {
         private final int user_id;
         private final String username;
         private final String encrypted_textAndTag_verify;
@@ -131,7 +178,13 @@ public class SQLite {
         private final byte[] salt;
         private final int argon2Type;
         private final String last_login;
-        public UserValues(int user_id, String username, String encrypted_textAndTag_verify, byte[] iv_verify, byte[] salt, int argon2Type, String last_login){
+
+        public UserValues(int user_id, String username, 
+                        String encrypted_textAndTag_verify, 
+                        byte[] iv_verify, 
+                        byte[] salt, int argon2Type, 
+                        String last_login)
+        {
             this.user_id = user_id;
             this.username = username;
             this.encrypted_textAndTag_verify = encrypted_textAndTag_verify;
@@ -141,37 +194,45 @@ public class SQLite {
             this.last_login = last_login;
         }
 
-        public int getUser_id() {
+        public int getUser_id() 
+        {
             return user_id;
         }
 
-        public String getUsername() {
+        public String getUsername() 
+        {
             return username;
         }
 
-        public String getEncrypted_textAndTag_verify() {
+        public String getEncrypted_textAndTag_verify() 
+        {
             return encrypted_textAndTag_verify;
         }
 
-        public byte[] getIv_verify() {
+        public byte[] getIv_verify() 
+        {
             return iv_verify;
         }
 
-        public byte[] getSalt() {
+        public byte[] getSalt() 
+        {
             return salt;
         }
 
-        public int getArgon2Type() {
+        public int getArgon2Type() 
+        {
             return argon2Type;
         }
 
-        public String getLast_login() {
+        public String getLast_login() 
+        {
             return last_login;
         }
         
         }
 
-    public ArrayList<MdpValues> get_mdp(int user_id) throws SQLException {
+    public ArrayList<MdpValues> get_mdp(int user_id) throws SQLException 
+    {
         Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
         String sql = "SELECT * FROM passwords WHERE user_id = ?";
         PreparedStatement pstmt = co.prepareStatement(sql);
@@ -197,7 +258,8 @@ public class SQLite {
         return rv;
     }
 
-    public final class MdpValues {
+    public final class MdpValues 
+    {
         private final int password_id;
         private final int user_id;
         private final String website_name;
@@ -207,7 +269,11 @@ public class SQLite {
         private final byte[] iv_username;
         private final byte[] iv_password;
 
-        public MdpValues(int password_id, int user_id, String website_name, String url, String encrypted_username, String encrypted_password, byte[] iv_username, byte[] iv_password){
+        public MdpValues(int password_id, int user_id, 
+                        String website_name, String url, 
+                        String encrypted_username, String encrypted_password, 
+                        byte[] iv_username, byte[] iv_password)
+        {
             this.password_id = password_id;
             this.user_id = user_id;
             this.website_name = website_name;
@@ -218,40 +284,53 @@ public class SQLite {
             this.iv_password = iv_password;
         }
 
-        public int getPassword_id() {
+        public int getPassword_id() 
+        {
             return this.password_id;
         }
 
-        public int getUser_id() {
+        public int getUser_id() 
+        {
             return this.user_id;
         }
 
-        public String getWebsite_name() {
+        public String getWebsite_name() 
+        {
             return this.website_name;
         }
 
-        public String getUrl() {
+        public String getUrl() 
+        {
             return this.url;
         }
 
-        public String getEncrypted_username() {
+        public String getEncrypted_username() 
+        {
             return this.encrypted_username;
         }
 
-        public String getEncrypted_password() {
+        public String getEncrypted_password() 
+        {
             return this.encrypted_password;
         }
 
-        public byte[] getIv_username() {
+        public byte[] getIv_username() 
+        {
             return this.iv_username;
         }
 
-        public byte[] getIv_password() {
+        public byte[] getIv_password() 
+        {
             return this.iv_password;
         }
     }
 
-    public void suppr_utilisateur(String username) throws SQLException { //On supprime un utilisateur via son username
+
+
+
+    //On supprime un utilisateur via son username
+    public void suppr_utilisateur(String username) throws SQLException 
+    { 
         Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
         String sql = "DELETE FROM users WHERE username = ?";
         PreparedStatement pstmt = co.prepareStatement(sql);
@@ -262,8 +341,12 @@ public class SQLite {
         co.close();
         //System.out.println("utilisateur supprimé avec succès");
     }
-///enlever static
-    public static void suppr_mdp(int user_id,String website_name) throws SQLException { //On supprime un utilisateur via son username
+
+
+    //enlever static
+    //On supprime un utilisateur via son username
+    public void suppr_mdp(int user_id,String website_name) throws SQLException 
+    { 
         Connection co = DriverManager.getConnection("jdbc:sqlite:base.db");
         ///String sql = "DELETE FROM passwords AS p JOIN users AS u ON u.user_id = p.user_id AND p.website_name = ?";
         String sql = "DELETE FROM passwords WHERE website_name = ? AND user_id = ?;";
